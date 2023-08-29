6f016bb14c2af62572c3db1802949ba4c7803b79
DantePc <djkmecdgm65@gmail.com>
2023-08-29 01:19:58 -0500

    comentando codigo

--- src/db.js
+++ src/db.js
@@ -19,11 +19,11 @@ const NAME = DB_NAME || DATABASE_PGDATABASE
 const sequelize = new Sequelize(DB_RENDER, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true,
-    }
-  }
+  //dialectOptions: {
+  //  ssl: {
+  //    require: true,
+  //  }
+  //}
 });
 
 const basename = path.basename(__filename);
